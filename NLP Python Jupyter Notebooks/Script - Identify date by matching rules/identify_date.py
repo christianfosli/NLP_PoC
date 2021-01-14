5f85bd96cde28d9c1a9e6046473c2a446d3942fb
Lars Berg Hustveit <lars-berg.hustveit@capgemini.com>
2021-01-15 00:03:03 +0100

    Created function that will take list of spacy lines as input and identify DATE. #15

--- NLP Python Jupyter Notebooks/Script - Identify date by matching rules/identify_date.py
+++ NLP Python Jupyter Notebooks/Script - Identify date by matching rules/identify_date.py
@@ -17,10 +17,18 @@ def identify_date_in_text(text):
 
     result = []
 
-    for match_id, start, end in matcher(doc):
+    for match_id, token_start, token_end in matcher(doc):
+
+        # convert token_span to char_span.
+        # char_span is needed to display correctly withdisplacy.render().
+        span = doc[token_start:token_end]
+        span_char_start = span[0].idx
+        span_char_end = span[-1].idx + len(span[-1].text)
 
         match_id_as_string = nlp.vocab.strings[match_id]
-        identified_entity = {'start': int(start), 'end': int(end), 'label': match_id_as_string}
+
+        identified_entity = {'start': span_char_start, 'end': span_char_end, 'label': match_id_as_string}
+
         result.append(identified_entity)
 
     return result
@@ -38,3 +46,29 @@ def merge_spacy_entity_results_to_spacy_ner_format(spacy_ner_formated_text_line,
         ents = spacy_ner_entities_to_be_merged_in_as_a_list
 
     return {'text': text, 'ents': sorted(ents, key=lambda x: x['start']), 'title': title}
+
+def identify_date_in_spacy_lines(spacy_lines):
+
+    result = []
+
+    for spacy_line in spacy_lines:
+
+        if spacy_line['text'] != "\n" and len(spacy_line['text']) > 0:
+
+            identify_date_in_text_result_in_a_list = identify_date_in_text(spacy_line['text'])
+
+            # this result might be empty.
+
+            if identify_date_in_text_result_in_a_list:
+
+                # we have results to merge in.
+                # merge in the result using this function.
+                new_spacy_line = merge_spacy_entity_results_to_spacy_ner_format(spacy_line,identify_date_in_text_result_in_a_list)
+
+                result.append(new_spacy_line)
+            else:
+                result.append(spacy_line)
+        else:
+            result.append(spacy_line)
+
+    return result
