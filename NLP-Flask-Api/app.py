f04aed6c00e74cc83249db6320f249c235a0a38d
Lars Berg Hustveit <lars-berg.hustveit@capgemini.com>
2021-01-15 12:39:32 +0100

    First test of python script in rest api works. #17

--- NLP-Flask-Api/app.py
+++ NLP-Flask-Api/app.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from identify_date import identify_date_in_text
+
 
 app = Flask(__name__)
 
 
 @app.route('/healthz')
 def health():
-    return 'OK'
+    result_in_a_list = identify_date_in_text("(1) This chapter applies to vessels constructed after 2 January 1988.\n")
+    return { "result": result_in_a_list}
 
 
 if __name__ == '__main__':
-    app.run()
+    #app.run()
+    app.run(debug=True)
