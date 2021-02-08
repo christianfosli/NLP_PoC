b63940600ab71c48eefbdf6808efc5bc8dbade98
Lars Berg Hustveit <lars-berg.hustveit@capgemini.com>
2021-02-08 15:22:25 +0100

    Multiple statments in one line supported. #17

--- NLP Python Jupyter Notebooks/Script - Api - identify-electrical-installation-in-text-service-norwegian-chapter-input/api_controller_identify_electrical_installation_in_text_service_norwegian_chapter_input.py
+++ NLP Python Jupyter Notebooks/Script - Api - identify-electrical-installation-in-text-service-norwegian-chapter-input/api_controller_identify_electrical_installation_in_text_service_norwegian_chapter_input.py
@@ -10,61 +10,79 @@ def create_api_response_for_post_identify_electrical_installation_in_text_servic
 
     for line in forward_filtered_result_with_only_the_things_we_are_looking_for:
 
+        # A line is to be interpreted as one sentence.
+
+        # We look for statments about something that starts with WATER_VESSEL and ends with VOLTAGE.
+
+        # Detection of multiple statments in one line is supported.
+
+        # Data
         text_service_url = line['title']
         text = line['text']
         ents = line['ents']
 
-        # result
-        result_length_dictionary = {}
-        result_length_prefix_dictionary = {}
+        # Metadata from URL
+        metadata_from_url = {}
         result_text_service_url = get_data_from_text_service_item_url(text_service_url)
+        if "regularion_year" in result_text_service_url:
+            metadata_from_url['regularion_year'] = result_text_service_url['regularion_year']
+        if "regularion_month" in result_text_service_url:
+            metadata_from_url['regularion_month'] = result_text_service_url['regularion_month']
+        if "regularion_day" in result_text_service_url:
+            metadata_from_url['regularion_day'] = result_text_service_url['regularion_day']
+        if "regularion_id" in result_text_service_url:
+            metadata_from_url['regularion_id'] = result_text_service_url['regularion_id']
+        if "chapter_number" in result_text_service_url:
+            metadata_from_url['chapter_number'] = result_text_service_url['chapter_number']
+        if "section_number" in result_text_service_url:
+            metadata_from_url['section_number'] = result_text_service_url['section_number']
+        if "part_number" in result_text_service_url:
+            metadata_from_url['part_number'] = result_text_service_url['part_number']
+        if "sub_part_number" in result_text_service_url:
+            metadata_from_url['sub_part_number'] = result_text_service_url['sub_part_number']
 
-        for ent in ents:
+        #This will be reset on detection of START ent and END ent.
+        temp_detection_dictionary = {}
+        temp_term_between_start_and_end_detected = False
+
+        for ent_id, ent in enumerate(ents):
 
             ent_label = ent['label']
             ent_start = ent['start']
             ent_end = ent['end']
             ent_text = text[ent_start:ent_end]
-
             ent_doc = nlp(ent_text)
             words_in_doc_count = len(ent_doc)
 
-            data = {}
+            if ent_label == "WATER_VESSEL":
+                # This is the START of a statment.
+                # Therefore resetting detetection dictionary: 
+                temp_detection_dictionary = {}
+                temp_term_between_start_and_end_detected = False
 
-            if ent_label == "VOLTAGE" and words_in_doc_count == 2:
-                # Example: 10,67 meter
-                data['voltage_value'] = ent_doc[0].text
-                data['measurement_text'] = ent_doc[1].text
+            elif ent_label == "ELECTRICAL_INSTALLATION":
+                temp_term_between_start_and_end_detected = True
 
-                if not result_length_dictionary:
-                    result_length_dictionary = data
-
-            elif ent_label == "LENGTH_PREFIX":
+            elif ent_label == "VOLTAGE_PREFIX":
                 # Example: mindre enn
-                data['length_prefix'] = ent_doc.text
+                temp_detection_dictionary['voltage_prefix'] = ent_doc.text
 
-                if not result_length_prefix_dictionary:
-                    result_length_prefix_dictionary = data
+            elif ent_label == "VOLTAGE" and words_in_doc_count == 2:
+                # This is the END of a statment.
 
-        merged_line_result = result_length_dictionary | result_length_prefix_dictionary
+                if temp_term_between_start_and_end_detected == True:
+                    # Statment is complete.
+                    # Adding result to output list.
 
-        if "regularion_year" in result_text_service_url:
-            merged_line_result['regularion_year'] = result_text_service_url['regularion_year']
-        if "regularion_month" in result_text_service_url:
-            merged_line_result['regularion_month'] = result_text_service_url['regularion_month']
-        if "regularion_day" in result_text_service_url:
-            merged_line_result['regularion_day'] = result_text_service_url['regularion_day']
-        if "regularion_id" in result_text_service_url:
-            merged_line_result['regularion_id'] = result_text_service_url['regularion_id']
-        if "chapter_number" in result_text_service_url:
-            merged_line_result['chapter_number'] = result_text_service_url['chapter_number']
-        if "section_number" in result_text_service_url:
-            merged_line_result['section_number'] = result_text_service_url['section_number']
-        if "part_number" in result_text_service_url:
-            merged_line_result['part_number'] = result_text_service_url['part_number']
-        if "sub_part_number" in result_text_service_url:
-            merged_line_result['sub_part_number'] = result_text_service_url['sub_part_number']
+                    # Example: 10,67 meter
+                    temp_detection_dictionary['voltage_value'] = ent_doc[0].text
+                    temp_detection_dictionary['measurement_text'] = ent_doc[1].text
 
-        forward_result.append(merged_line_result)
+                    detection_with_url_metadata = temp_detection_dictionary | metadata_from_url
+                    forward_result.append(detection_with_url_metadata)
+
+                # Resetting detetection dictionary.
+                temp_detection_dictionary = {}
+                temp_term_between_start_and_end_detected = False
 
     return forward_result
