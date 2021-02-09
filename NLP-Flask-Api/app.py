07b972c5e3a44ff07bd1a8d5cdc446f03f6f5958
Lars Berg Hustveit <lars-berg.hustveit@capgemini.com>
2021-02-09 15:18:39 +0100

    Added norwegian date module to NLP API. #17

--- NLP-Flask-Api/app.py
+++ NLP-Flask-Api/app.py
@@ -7,6 +7,7 @@ from transform_text_service_input_to_spacy_format import transform_chapter_from_
 # spacy matching rule
 from spacy_matching_rule_identify_vessel_length_overall_no import identify_length_overall_in_spacy_lines
 from spacy_matching_rule_identify_electrical_installation_no import identify_electrical_installation_in_spacy_lines
+from spacy_matching_rule_identify_date_no import identify_date_in_norwegian_spacy_lines
 
 # api controller
 from api_controller_identify_vessel_length_overall import create_api_response_for_post_identify_vessel_length_overall_in_text_service_norwegian_chapter_input
@@ -75,6 +76,18 @@ def post_identify_electrical_installation_in_text_service_norwegian_chapter_inpu
     forward_filtered_result_with_only_the_things_we_are_looking_for = [spacy_line for spacy_line in forward_result_with_electrical_installation if any("VOLTAGE" == x['label'] for x in spacy_line['ents']) and any("WATER_VESSEL" == x['label'] for x in spacy_line['ents']) and any("ELECTRICAL_INSTALLATION" == x['label'] for x in spacy_line['ents'])]
     forward_api_response = create_api_response_for_post_identify_electrical_installation_in_text_service_norwegian_chapter_input(forward_filtered_result_with_only_the_things_we_are_looking_for)
     return jsonify({"identified_electrical_installations": forward_api_response})
+
+# Documentation: https://sdir.atlassian.net/wiki/spaces/SDIR/pages/1225359396/identify-build-date-in-text-service-norwegian-chapter-input
+@app.route("/identify-build-date-in-text-service-norwegian-chapter-input", methods=["POST"])
+def post_identify_build_date_in_text_service_norwegian_chapter_input():
+    input_chapter_text_as_json_in_text_service_format = request.json
+    forward_text_transformed_to_spacy_format = transform_chapter_from_text_service_to_spacy_format(input_chapter_text_as_json_in_text_service_format)
+    forward_result_with_date_in_norwegian = identify_date_in_norwegian_spacy_lines(forward_text_transformed_to_spacy_format)
+
+
+
+
+    return jsonify({"identified_build_dates": []})
 
 if __name__ == '__main__':
     #app.run()
