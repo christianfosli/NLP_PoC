ba80b8f1bea9b65c489be06bbe27b4cdf5b3ae7e
Lars Berg Hustveit <lars-berg.hustveit@capgemini.com>
2021-02-19 14:42:16 +0100

    Add to API. #5

--- NLP-Flask-Api/app.py
+++ NLP-Flask-Api/app.py
@@ -12,6 +12,8 @@ from spacy_matching_rule_identify_electrical_installation_no import identify_ele
 # build_date
 from spacy_matching_rule_identify_build_date_no import identify_build_date_in_norwegian_spacy_lines
 from spacy_matching_rule_identify_build_date_en import identify_build_date_in_english_spacy_lines
+# alternative_reference
+from spacy_matching_rule_identify_alternative_reference_no import identify_alternative_reference_in_norwegian_spacy_lines
 
 #
 # API controllers
@@ -24,6 +26,8 @@ from api_controller_identify_electrical_installation_in_text_service_norwegian_c
 # build_date
 from api_controller_identify_build_date_in_text_service_norwegian_chapter_input import create_api_response_for_post_identify_build_date_in_text_service_norwegian_chapter_input
 from api_controller_identify_build_date_in_text_service_english_chapter_input import create_api_response_for_post_identify_build_date_in_text_service_english_chapter_input
+# alternative_reference
+from api_controller_identify_alternative_reference_in_text_service_norwegian_chapter_input import create_api_response_for_post_identify_alternative_reference_in_text_service_norwegian_chapter_input
 
 app = Flask(__name__)
 
@@ -71,6 +75,16 @@ def post_identify_build_date_in_text_service_english_chapter_input():
     forward_filtered_result_with_only_the_things_we_are_looking_for = [spacy_line for spacy_line in forward_result_with_date_in_english if any("WATER_VESSEL" == x['label'] for x in spacy_line['ents']) and any("CONSTRUCT" == x['label'] for x in spacy_line['ents']) and any("DATE" == x['label'] for x in spacy_line['ents'])]
     forward_api_response = create_api_response_for_post_identify_build_date_in_text_service_english_chapter_input(forward_filtered_result_with_only_the_things_we_are_looking_for)
     return jsonify({"identified_build_dates": forward_api_response})
+
+# Documentation: https://sdir.atlassian.net/wiki/spaces/SDIR/pages/1264615425/identify-alternative-reference-in-text-service-norwegian-chapter-input
+@app.route("/identify-alternative-reference-in-text-service-norwegian-chapter-input", methods=["POST"])
+def post_identify_alternative_reference_in_text_service_norwegian_chapter_input():
+    input_chapter_text_as_json_in_text_service_format = request.json
+    forward_text_transformed_to_spacy_format = transform_chapter_from_text_service_to_spacy_format(input_chapter_text_as_json_in_text_service_format)
+    forward_result_with_date_in_norwegian = identify_alternative_reference_in_norwegian_spacy_lines(forward_text_transformed_to_spacy_format)
+    forward_filtered_result_with_only_the_things_we_are_looking_for = [spacy_line for spacy_line in forward_result_with_date_in_norwegian if any("ALTERNATIVE_REFERENCE" == x['label'] for x in spacy_line['ents'])]
+    forward_api_response = create_api_response_for_post_identify_alternative_reference_in_text_service_norwegian_chapter_input(forward_filtered_result_with_only_the_things_we_are_looking_for)
+    return jsonify({"identified_alternative-reference": forward_api_response})
 
 if __name__ == '__main__':
     #app.run()
