692b5e696b768c4e90b1acd537da678bb14c5782
Lars Berg Hustveit <lars-berg.hustveit@capgemini.com>
2021-02-24 16:27:50 +0100

    FLASHPOINT - Created API #30

--- NLP-Flask-Api/app.py
+++ NLP-Flask-Api/app.py
@@ -18,6 +18,7 @@ from spacy_matching_rule_identify_alternative_reference_no import identify_alter
 from spacy_matching_rule_identify_PASSENGER_no import identify_PASSENGER_in_norwegian_spacy_lines
 from spacy_matching_rule_identify_GROSS_TONNAGE_no import identify_GROSS_TONNAGE_in_norwegian_spacy_lines
 from spacy_matching_rule_identify_VESSEL_no import identify_VESSEL_in_norwegian_spacy_lines
+from spacy_matching_rule_identify_FLASHPOINT_no import identify_FLASHPOINT_in_norwegian_spacy_lines
 
 #
 # API controllers
@@ -36,6 +37,7 @@ from api_controller_identify_alternative_reference_in_text_service_norwegian_cha
 from api_controller_identify_PASSENGER_in_text_service_norwegian_chapter import create_api_response_for_post_identify_PASSENGER_in_text_service_norwegian_chapter
 from api_controller_identify_GROSS_TONNAGE_in_text_service_norwegian_chapter import create_api_response_for_post_identify_GROSS_TONNAGE_in_text_service_norwegian_chapter
 from api_controller_identify_VESSEL_in_text_service_norwegian_chapter import create_api_response_for_post_identify_VESSEL_in_text_service_norwegian_chapter
+from api_controller_identify_FLASHPOINT_in_text_service_norwegian_chapter import create_api_response_for_post_identify_FLASHPOINT_in_text_service_norwegian_chapter
 
 app = Flask(__name__)
 
@@ -123,6 +125,16 @@ def post_identify_VESSEL_in_text_service_norwegian_chapter():
     forward_filtered_result_with_only_the_things_we_are_looking_for = [spacy_line for spacy_line in forward_result_with_date_in_norwegian if any("VESSEL" == x['label'] for x in spacy_line['ents'])]
     forward_api_response = create_api_response_for_post_identify_VESSEL_in_text_service_norwegian_chapter(forward_filtered_result_with_only_the_things_we_are_looking_for)
     return jsonify({"identified_VESSEL": forward_api_response})
+
+# Documentation: https://sdir.atlassian.net/wiki/spaces/SDIR/pages/1282113555/flashpoint+-+no
+@app.route("/identify-FLASHPOINT-in-text-service-norwegian-chapter", methods=["POST"])
+def post_identify_FLASHPOINT_in_text_service_norwegian_chapter():
+    input_chapter_text_as_json_in_text_service_format = request.json
+    forward_text_transformed_to_spacy_format = transform_chapter_from_text_service_to_spacy_format(input_chapter_text_as_json_in_text_service_format)
+    forward_result_with_date_in_norwegian = identify_FLASHPOINT_in_norwegian_spacy_lines(forward_text_transformed_to_spacy_format)
+    forward_filtered_result_with_only_the_things_we_are_looking_for = [spacy_line for spacy_line in forward_result_with_date_in_norwegian if any("FLASHPOINT" == x['label'] for x in spacy_line['ents'])]
+    forward_api_response = create_api_response_for_post_identify_FLASHPOINT_in_text_service_norwegian_chapter(forward_filtered_result_with_only_the_things_we_are_looking_for)
+    return jsonify({"identified_FLASHPOINT": forward_api_response})
 
 if __name__ == '__main__':
     #app.run()
