679833a03529e87b9769f7c9f27fd3e8e17cd505
Lars Berg Hustveit <lars-berg.hustveit@capgemini.com>
2021-02-25 17:20:26 +0100

    MOBILE_UNIT - Created API #30

---
+++ NLP-Flask-Api/api_controller_identify_MOBILE_UNIT_in_text_service_norwegian_chapter.py
@@ -0,0 +1,66 @@
+import spacy
+from spacy.lang.en import English
+from transform_text_service_input_to_spacy_format import get_data_from_text_service_item_url
+
+def create_api_response_for_post_identify_MOBILE_UNIT_in_text_service_norwegian_chapter(forward_filtered_result_with_only_the_things_we_are_looking_for):
+
+    nlp = English()
+    forward_result = []
+
+    for line in forward_filtered_result_with_only_the_things_we_are_looking_for:
+
+        # Get NLP data from line
+        text_service_url = line['title']
+        text = line['text']
+        ents = line['ents'] # discovered enteties in the line
+        last_index_number_of_ents = len(ents)-1
+        doc = nlp(text)
+
+        # Get metadata from URL
+        result_text_service_url = get_data_from_text_service_item_url(text_service_url)
+        metadata_from_url = {}
+        if "regulation_year" in result_text_service_url:
+            metadata_from_url['regulation_year'] = result_text_service_url['regulation_year']
+        if "regulation_month" in result_text_service_url:
+            metadata_from_url['regulation_month'] = result_text_service_url['regulation_month']
+        if "regulation_day" in result_text_service_url:
+            metadata_from_url['regulation_day'] = result_text_service_url['regulation_day']
+        if "regulation_id" in result_text_service_url:
+            metadata_from_url['regulation_id'] = result_text_service_url['regulation_id']
+        if "chapter_number" in result_text_service_url:
+            metadata_from_url['chapter_number'] = result_text_service_url['chapter_number']
+        if "section_number" in result_text_service_url:
+            metadata_from_url['section_number'] = result_text_service_url['section_number']
+        if "part_number" in result_text_service_url:
+            metadata_from_url['part_number'] = result_text_service_url['part_number']
+        if "sub_part_number" in result_text_service_url:
+            metadata_from_url['sub_part_number'] = result_text_service_url['sub_part_number']
+
+        # For each ent in line
+        for ent_index_number, ent in enumerate(ents):
+
+            ent_label = ent['label']
+            ent_start = ent['start']
+            ent_end = ent['end']
+
+            ent_text = text[ent_start:ent_end] # same as: doc[ent_token_span.start:ent_token_span.end]
+            ent_doc = nlp(ent_text)
+            words_in_doc_count = len(ent_doc)
+
+            ent_token_span = doc.char_span(ent_start,ent_end)
+            ent_token_span_start = ent_token_span.start
+            ent_token_span_end = ent_token_span.end
+            
+            #print(ent_text + " - " + ent_label + " (" + str(ent_token_span_start) + ":" + str(ent_token_span_end) + ")")
+
+            #
+            # Statment concluder
+            #
+
+            if ent_label == "MOBILE_UNIT":
+
+                detection_with_url_metadata = dict(metadata_from_url)
+                detection_with_url_metadata["mobile_unit_text"] = ent_text
+                forward_result.append(detection_with_url_metadata)
+
+    return forward_result
--- NLP-Flask-Api/app.py
+++ NLP-Flask-Api/app.py
@@ -20,6 +20,7 @@ from spacy_matching_rule_identify_GROSS_TONNAGE_no import identify_GROSS_TONNAGE
 from spacy_matching_rule_identify_VESSEL_no import identify_VESSEL_in_norwegian_spacy_lines
 from spacy_matching_rule_identify_FLASHPOINT_no import identify_FLASHPOINT_in_norwegian_spacy_lines
 from spacy_matching_rule_identify_VESSEL_TYPE_no import identify_VESSEL_TYPE_in_norwegian_spacy_lines
+from spacy_matching_rule_identify_MOBILE_UNIT_no import identify_MOBILE_UNIT_in_norwegian_spacy_lines
 
 #
 # API controllers
@@ -40,6 +41,7 @@ from api_controller_identify_GROSS_TONNAGE_in_text_service_norwegian_chapter imp
 from api_controller_identify_VESSEL_in_text_service_norwegian_chapter import create_api_response_for_post_identify_VESSEL_in_text_service_norwegian_chapter
 from api_controller_identify_FLASHPOINT_in_text_service_norwegian_chapter import create_api_response_for_post_identify_FLASHPOINT_in_text_service_norwegian_chapter
 from api_controller_identify_VESSEL_TYPE_in_text_service_norwegian_chapter import create_api_response_for_post_identify_VESSEL_TYPE_in_text_service_norwegian_chapter
+from api_controller_identify_MOBILE_UNIT_in_text_service_norwegian_chapter import create_api_response_for_post_identify_MOBILE_UNIT_in_text_service_norwegian_chapter
 
 app = Flask(__name__)
 
@@ -147,6 +149,16 @@ def post_identify_VESSEL_TYPE_in_text_service_norwegian_chapter():
     forward_filtered_result_with_only_the_things_we_are_looking_for = [spacy_line for spacy_line in forward_result_with_date_in_norwegian if any("VESSEL_TYPE" == x['label'] for x in spacy_line['ents'])]
     forward_api_response = create_api_response_for_post_identify_VESSEL_TYPE_in_text_service_norwegian_chapter(forward_filtered_result_with_only_the_things_we_are_looking_for)
     return jsonify({"identified_VESSEL_TYPE": forward_api_response})
+
+# Documentation: 
+@app.route("/identify-MOBILE-UNIT-in-text-service-norwegian-chapter", methods=["POST"])
+def post_identify_MOBILE_UNIT_in_text_service_norwegian_chapter():
+    input_chapter_text_as_json_in_text_service_format = request.json
+    forward_text_transformed_to_spacy_format = transform_chapter_from_text_service_to_spacy_format(input_chapter_text_as_json_in_text_service_format)
+    forward_result_with_date_in_norwegian = identify_MOBILE_UNIT_in_norwegian_spacy_lines(forward_text_transformed_to_spacy_format)
+    forward_filtered_result_with_only_the_things_we_are_looking_for = [spacy_line for spacy_line in forward_result_with_date_in_norwegian if any("MOBILE_UNIT" == x['label'] for x in spacy_line['ents'])]
+    forward_api_response = create_api_response_for_post_identify_MOBILE_UNIT_in_text_service_norwegian_chapter(forward_filtered_result_with_only_the_things_we_are_looking_for)
+    return jsonify({"identified_MOBILE_UNIT": forward_api_response})
 
 if __name__ == '__main__':
     #app.run()
