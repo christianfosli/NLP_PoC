b68eff0ab815066c4ffeb1cbcccd7a45567ffe99
Lars Berg Hustveit <lars-berg.hustveit@capgemini.com>
2021-03-01 14:34:46 +0100

    CONVERSION - Created API #30

--- NLP-Flask-Api/app.py
+++ NLP-Flask-Api/app.py
@@ -24,6 +24,7 @@ from spacy_matching_rule_identify_MOBILE_UNIT_no import identify_MOBILE_UNIT_in_
 from spacy_matching_rule_identify_CARGO_no import identify_CARGO_in_norwegian_spacy_lines
 from spacy_matching_rule_identify_TRADE_AREA_no import identify_TRADE_AREA_in_norwegian_spacy_lines
 from spacy_matching_rule_identify_RADIO_AREA_no import identify_RADIO_AREA_in_norwegian_spacy_lines
+from spacy_matching_rule_identify_CONVERSION_no import identify_CONVERSION_in_norwegian_spacy_lines
 
 #
 # API controllers
@@ -48,6 +49,7 @@ from api_controller_identify_MOBILE_UNIT_in_text_service_norwegian_chapter impor
 from api_controller_identify_CARGO_in_text_service_norwegian_chapter import create_api_response_for_post_identify_CARGO_in_text_service_norwegian_chapter
 from api_controller_identify_TRADE_AREA_in_text_service_norwegian_chapter import create_api_response_for_post_identify_TRADE_AREA_in_text_service_norwegian_chapter
 from api_controller_identify_RADIO_AREA_in_text_service_norwegian_chapter import create_api_response_for_post_identify_RADIO_AREA_in_text_service_norwegian_chapter
+from api_controller_identify_CONVERSION_in_text_service_norwegian_chapter import create_api_response_for_post_identify_CONVERSION_in_text_service_norwegian_chapter
 
 app = Flask(__name__)
 
@@ -195,6 +197,16 @@ def post_identify_RADIO_AREA_in_text_service_norwegian_chapter():
     forward_filtered_result_with_only_the_things_we_are_looking_for = [spacy_line for spacy_line in forward_result_with_date_in_norwegian if any("RADIO_AREA" == x['label'] for x in spacy_line['ents']) and any("RADIO_AREA_TYPE" == x['label'] for x in spacy_line['ents'])]
     forward_api_response = create_api_response_for_post_identify_RADIO_AREA_in_text_service_norwegian_chapter(forward_filtered_result_with_only_the_things_we_are_looking_for)
     return jsonify({"identified_RADIO_AREA": forward_api_response})
+
+# Documentation: https://sdir.atlassian.net/wiki/spaces/SDIR/pages/1300561935/conversion+-+no
+@app.route("/identify-CONVERSION-in-text-service-norwegian-chapter", methods=["POST"])
+def post_identify_CONVERSION_in_text_service_norwegian_chapter():
+    input_chapter_text_as_json_in_text_service_format = request.json
+    forward_text_transformed_to_spacy_format = transform_chapter_from_text_service_to_spacy_format(input_chapter_text_as_json_in_text_service_format)
+    forward_result_with_date_in_norwegian = identify_CONVERSION_in_norwegian_spacy_lines(forward_text_transformed_to_spacy_format)
+    forward_filtered_result_with_only_the_things_we_are_looking_for = [spacy_line for spacy_line in forward_result_with_date_in_norwegian if any("CONVERSION" == x['label'] for x in spacy_line['ents'])]
+    forward_api_response = create_api_response_for_post_identify_CONVERSION_in_text_service_norwegian_chapter(forward_filtered_result_with_only_the_things_we_are_looking_for)
+    return jsonify({"identified_CONVERSION": forward_api_response})
 
 if __name__ == '__main__':
     #app.run()
