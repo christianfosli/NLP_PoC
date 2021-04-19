8ae443f2a951a83a97240cc49d5de787f0c16ba6
Lars Berg Hustveit <lars.at.cap@gmail.com>
2021-04-19 15:03:52 +0200

    Created script that will find the titles. #58

---
+++ NLP Python Jupyter Notebooks/Script - Create title dictionary from text service input/create_title_dictionary_from_text_service_input.py
@@ -0,0 +1,15 @@
+def create_dictionary(chapter_title_as_string,text_service_chapter_input_sections):
+
+    result = {}
+    result["chapter_title"] = chapter_title_as_string
+
+    section_title_in_dictionary = {}
+    for section_item in text_service_chapter_input_sections:
+        section_title = section_item['title']
+        for subsection_item in section_item['parts']:
+            section_title_in_dictionary[subsection_item['url']] = section_title
+            for sentence_item in subsection_item['subparts']:
+                section_title_in_dictionary[sentence_item['url']] = section_title
+
+    result["section_title_in_dictionary"] = section_title_in_dictionary
+    return result
